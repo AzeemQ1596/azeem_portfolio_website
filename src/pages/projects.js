03156a629e96bf5a4d691afedc10780df08e08e6
AzeemQ1596 <azeem.quadri99@gmail.com>
2022-03-15 17:04:50 -0400

    edited some of the project descriptions

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -22,17 +22,17 @@ export default function Projects() {
                     </div>
                     
                     <p>
-                    This is probably one of the most proud projects I've ever done (yet!). This project was done for our capstone project. The main idea of the project was to make a code tracking tool for developers who use the <a href="https://segment.com/" target="_blank" rel="noopener noreferrer" aria-hidden="true">Segment</a> API, a customer data platform. Segment is used by developers to see how their customers interact with their website. As their website grows bigger, more Segment API calls are added, which makes it harder to keep track of them, especially for developers.
+                    This is probably one of my most proud projects I've ever done (yet!). This was done for our Capstone project. The main idea of the project was to make a code tracking tool for developers who use the <a href="https://segment.com/" target="_blank" rel="noopener noreferrer" aria-hidden="true">Segment</a> API, a customer data platform. Segment is used by developers to see how their customers interact with their website. As their website grows bigger, more Segment API calls are added, which makes it harder to keep track of them, especially for developers.
                     </p>
                     <p>
                     We use regular expressions (RegEx) to do a complex and smart search in the user's codebase for all Segment API calls, and collect them in present them in a table right inside the IDE! This table shows information like line number, file name and other meta data of the Segment call. From here, developers can double-click on the row which will open the document inside the IDE and highlight the line where this is present. Users can also sort, filter the rows in the table too. A cool feature of this table is that it has dynamic theme switching, that automatically switches between light, dark and high-contrast themes depending on the user's IDE theme for better visual clarity. 
                     </p>
 
                     <p>
-                        Another feature of the extension is to is that it allows users to reuse previously used API calls from the table. Overall this greatly improved the developer experiences and to remove some of the mundane and tedious tasks in using the Segment API.
+                        Another feature of the extension is that it allows users to reuse previously used API calls from the table. Overall this greatly improved developer experience and has removed some of the mundane and tedious tasks when using the Segment API.
                     </p>
                     <p>
-                        Now I know what you're all thinking: "But why not do a simple Cmd+f/Ctrl+f search to locate the Segment API calls??" Well, user's not only have to track these API calls, but also see at a glance where all the API calls are present and the types of present. And as mentioned earlier, as the website grows bigger, more API calls are needed, and it becomes harder to keep track of all the API calls.
+                        Now I know what you're all thinking: "But why not do a simple Cmd+f/Ctrl+f search to locate the Segment API calls??" Well, user's not only have to track these API calls, but also see at a glance where all the API calls are present and the types of API calls present. And as mentioned earlier, as the website grows bigger, more API calls are needed, and it becomes harder to keep track of all the API calls.
                     </p>
 
                 </div> 
@@ -49,7 +49,7 @@ export default function Projects() {
                     <p> This was a machine learning project to classify between spam and ham emails (yes non-spam emails are called ham emails). I used Python and machine learning libraries like TensorFlow and Numpy. I used the <a href="https://github.com/erayon/Email-spam-filter-naive-bayes-classifier-scikit-learn-text-classification/tree/master/CSDMC2010_SPAM/CSDMC2010_SPAM">CSDMC2010 SPAM corpus </a> which contains 4327 training and 4292 testing data. </p>
 
                     <p>
-                    The data required a lot of preprocessing before any training could be done, like removing all the HTML tags, subject tags, stop words and punctuation. Then finally I was able to implement a RNN (recurrent neural network) with LTSM layers to train the model. I was able to achieve 90% testing accuracy.
+                    The data required a lot of preprocessing before any training could be done, like removing all the HTML tags, subject tags, stop words and punctuation. Then finally I was able to implement a RNN (recurrent neural network) with LTSM layers to train the model. The model achieved 90% testing accuracy.
                     </p>
                 </div> 
                 <div className="project-col">
@@ -82,10 +82,10 @@ export default function Projects() {
 
                         <a href="https://docs.google.com/document/d/e/2PACX-1vQvbWkCRYYs3UFzJiz-52v4in-WZyoy7g3uSntzdmOHD1kiDtMytURCeSQ-Bd0i2tV-dzJMu_WfFPPW/pub" className="fa-solid fa-file-pdf" target="_blank" rel="noopener noreferrer" aria-hidden="true"></a>
                     </div>
-                    <p> This was a machine learning group project to classify images of recyclable and organic waste. This project was made using Python, TensorFlow and Numpy. We got our dataset from Kaggle, which contains 2513 images. From here we were able to do the necessary preprocessing steps, like resizing the images and converting the images to suitable to be read by our model.</p>
+                    <p> This was a machine learning group project to classify images of recyclable and organic waste. This project was made using Python, TensorFlow and Numpy. We got our dataset from Kaggle, which contains 2513 images. From here we were able to do the necessary preprocessing steps, like resizing the images and converting the images.</p>
 
                     <p>
-                    We implemented a convolutional neural network (which is best used for image classification) with pooling layers to train the model. We used the ADAM optimizer for gradient descent. Some steps to prevent overfitting was to random zoom and dropout layers. We achieved 87% accuracy on the testing data.
+                    We implemented a convolutional neural network (which is best used for image classification) with pooling layers to train the model. We used the ADAM optimizer for gradient descent.Random Zoom and Dropout Layers were added to prevent overfitting. Our model achieved 87% accuracy on the testing data.
                     </p>
                 </div> 
                

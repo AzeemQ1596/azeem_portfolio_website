6548be6d03c6b5905aaba02ffce0a5991925e9cb
AzeemQ1596 <azeem.quadri99@gmail.com>
2022-07-01 23:33:02 -0400

    Changed routing, 14th time

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import './Responsive.css';
 
 import Header from './pages/header'
+import Home from "./pages/home";
 import About from './pages/about'
 import Projects from "./pages/projects";
-import Home from "./pages/home";
 
 function App() {
   return (
 
       <div>
-        <Header />
+        <Header/>
         <Routes>
             
-            
+            <Route index element={<Home/>}/>
             <Route exact path="/azeem_portfolio_website" element={<Home/>} />
             <Route exact path='/about' element={<About/>} />
             <Route exact path='/projects' element={<Projects/>} />

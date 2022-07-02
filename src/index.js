f452add602758bb63e3747f9379c7172122bdc4a
AzeemQ1596 <azeem.quadri99@gmail.com>
2022-07-01 23:21:44 -0400

    Changed routing, 12th time

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter} from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App/>   
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
